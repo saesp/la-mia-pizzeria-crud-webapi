ff24ebd5c81db2b5a214fdef7994b445af60e84d
Salvatore Esposito <esposito.00@outlook.it>
2023-05-12 17:45:29 +0200

    added POST (Create) in APIController

--- la-mia-pizzeria-static/Controllers/API/ApiPizzaController.cs
+++ la-mia-pizzeria-static/Controllers/API/ApiPizzaController.cs
@@ -6,7 +6,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace la_mia_pizzeria_static.Controllers.API
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ApiPizzaController : ControllerBase //ControllerBase offre metodi e proprietà utili per la gestione delle richieste e risposte HTTP 
     { 
@@ -21,5 +21,52 @@ namespace la_mia_pizzeria_static.Controllers.API
                 return Ok(pizzas.ToList());
             }
         }
+
+
+        //POST
+        [HttpPost]
+        public IActionResult Create([FromBody] Pizza data)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza pizzaCreate = new Pizza();
+                pizzaCreate.Name = data.Name;
+                pizzaCreate.Description = data.Description;
+                pizzaCreate.Image = data.Image;
+                pizzaCreate.Price = data.Price;
+                pizzaCreate.CategoryId = data.CategoryId; //one to many
+                
+                context.Pizzas.Add(pizzaCreate);
+                context.SaveChanges();
+
+
+                return Ok();
+            }
+        }
+
+
+        ////PUT
+        //[HttpPut("{id}")]
+        //public IActionResult Update(int id, [FromBody] Pizza data)
+        //{
+        //    using (PizzaContext context = new PizzaContext())
+        //    {
+        //        // cerchiamo il dato
+        //        PizzaContext pizzaEdit = context.PizzaContexts
+        //        .Where(pizza => pizza.Id == id).FirstOrDefault();
+        //        if (pizzaEdit != null)
+        //        {
+        //            //---
+
+
+        //            return Ok();
+        //        }
+        //        else
+        //        {
+        //            // se non è stato trovato resituiamo che non esiste
+        //            return NotFound();
+        //        }
+        //    }
+        //}
     }
 }
